a417d107b3f0bbaf83fea28f3c21e5fbda097346
sutheeshsudalai96 <43032317+sutheeshsudalai96@users.noreply.github.com>
2018-09-07 11:48:23 +0530

    Add files via upload

---
+++ sutheesh.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,w,x,y,z;
+    a=25;
+    b=5;
+    y=a+b;
+    x=a-b;
+    w=a*b;
+    z=a/b;
+    System.out.println("sum"+y);
+    System.out.println("sub"+x);
+    System.out.println("dis"+z);
+    System.out.println("mul"+w);
+  }
+}
