e1179681d8be622ac6a783c28cae6e55dd3b5540
sutheeshsudalai96 <43032317+sutheeshsudalai96@users.noreply.github.com>
2018-09-07 12:25:25 +0530

    Add files via upload

---
+++ doubl.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    double b,w,x,y,z;
+    a=25;
+    b=75.65;
+    y=a+b;
+    x=a-b;
+    w=a*b;
+    z=a/b;
+    System.out.println("sum"+y);
+    System.out.println("sub"+x);
+    System.out.println("dis"+w);
+    System.out.println("dis"+z);
+  }
+}
