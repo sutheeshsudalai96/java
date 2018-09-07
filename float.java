a94097cbff424ea1763b40be3baa071e59868d24
sutheeshsudalai96 <43032317+sutheeshsudalai96@users.noreply.github.com>
2018-09-07 12:07:45 +0530

    Add files via upload

---
+++ float.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,w,x,y,z;
+    a=25;
+    b=5.65f;
+    y=a+b;
+    x=a-b;
+    w=a*b;
+    z=a/b;
+    System.out.println("sum"+y);
+    System.out.println("sub"+x);
+    System.out.println("dis"+w);
+    System.out.println("dis"+z);
+  }
+}
