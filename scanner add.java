f39dc06900c4e7ae690ad777615db9534ea46636
sutheeshsudalai96 <43032317+sutheeshsudalai96@users.noreply.github.com>
2018-09-07 14:46:08 +0530

    Add files via upload

---
+++ scanner add.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    int s,t,u,v;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the first value");
+    int a=sc.nextInt();
+    System.out.println("Enter the second value ");
+    int b=sc.nextInt();
+    System.out.println("Enter third the value ");
+    int c=sc.nextInt();
+    System.out.println("Enter the  fourth value ");
+    int d=sc.nextInt();
+    System.out.println("Enter the  fifth value ");
+    int e=sc.nextInt();
+    System.out.println("Enter the  sixth value ");
+    int f=sc.nextInt();
+    s=a+b+c+d+e+f;
+    t=a-b-c-d-e-f;
+    System.out.println("add"+s );
+    System.out.println("sub"+t );
+  }
+}
