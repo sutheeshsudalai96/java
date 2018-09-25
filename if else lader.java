8544eefea49b6cc75cfccba9f2107da694546c0c
sutheeshsudalai96 <43032317+sutheeshsudalai96@users.noreply.github.com>
2018-09-25 15:28:03 +0530

    Add files via upload

---
+++ if else lader.java
@@ -0,0 +1,17 @@
+class Main
+{
+public static void main(String args[])
+{
+int n=5000;
+if(n==1)
+System.out.println("Rs.20000 & Above –Free Trolley      Suitcase");
+else if(n==2)
+System.out.println("Rs.10000 & Above –Free Travel Bag");
+else if(n==3)
+System.out.println("Rs.5000 & Above –Free BackPck");
+else if(n==4)
+System.out.println("Below 5000 –Collect money even for cover we give");
+else
+System.out.println("Collect money even for cover we give");
+}
+}
