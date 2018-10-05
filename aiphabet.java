b739012654b4b7e99a0ab1ddcb62f71d5d60deba
sutheeshsudalai96 <43032317+sutheeshsudalai96@users.noreply.github.com>
2018-10-05 12:27:13 +0530

    Add files via upload

---
+++ aiphabet.java
@@ -0,0 +1,12 @@
+class Main  {
+
+    public static void main(String[] args) {
+
+        char c = '1';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
